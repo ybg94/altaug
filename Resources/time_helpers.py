dca4576a94257da906ea1cb936622963837364d1
Niirn <dreammagica@gmail.com>
2025-10-07 19:29:45 +0200

    add slider for pyautogui pause

---
+++ Resources/time_helpers.py
@@ -0,0 +1,16 @@
+import time
+import dearpygui.dearpygui as dpg
+from . import gui_tags 
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        should_log = dpg.get_value(gui_tags.PERFORMANCE_LOGGING_TAG)
+        if not should_log:
+            return func(*args, **kwargs)
+
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"{func.__name__} took {end - start:.6f} seconds")
+        return result
+    return wrapper
