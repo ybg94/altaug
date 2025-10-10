11ab5ad5a9d6337543b0be9a96e2657db900a979
Niirn <dreammagica@gmail.com>
2025-10-10 19:32:45 +0200

    regex library modal base

--- Resources/gui.py
+++ Resources/gui.py
@@ -63,6 +63,50 @@ def set_pyautogui_pause() -> None:
     logging.debug(f"Setting PAUSE to {value}.")
     pyautogui.PAUSE = value
 
+def init_regex_wizard_modal() -> None:
+    item_base_combo_items: dict[str, list[str]] = {
+        "Armour": ["Warlock Boots", "Lich's Circlet"],
+        "Weapon": ["Spine Bow"],
+        "Map": ["Tier 17", "Tier 16"],
+    }
+    regex_combo_items: dict[str, list[str]] = {
+        "Warlock Boots": ["One", "Two"],
+        "Lich's Circlet": ["Three"],
+        "Spine Bow": ["Four"],
+        "Tier 17": ["Five", "Six"],
+        "Tier 16": ["Seven"],
+    }
+
+    ITEM_TYPE_COMBO_DEFAULT = "Select item type..."
+    ITEM_BASE_COMBO_DEFAULT = "Select item base..."
+    REGEX_COMBO_DEFAULT = "Select RegEx preset..."
+
+    def item_type_selected() -> None:
+        selected_type: str = dpg.get_value(gui_tags.REGEX_WIZARD_MODAL_COMBO_TYPE_TAG)
+        dpg.configure_item(gui_tags.REGEX_WIZARD_MODAL_COMBO_BASE_TAG, show=True, items=item_base_combo_items[selected_type])
+        dpg.set_value(gui_tags.REGEX_WIZARD_MODAL_COMBO_BASE_TAG, ITEM_BASE_COMBO_DEFAULT)
+
+        # Reset dependant combos
+        dpg.configure_item(gui_tags.REGEX_WIZARD_MODAL_COMBO_REGEX_TAG, show=False, items=[])
+        dpg.set_value(gui_tags.REGEX_WIZARD_MODAL_COMBO_REGEX_TAG, REGEX_COMBO_DEFAULT)
+
+    def item_base_selected() -> None:
+        selected_base: str = dpg.get_value(gui_tags.REGEX_WIZARD_MODAL_COMBO_BASE_TAG)
+        dpg.configure_item(gui_tags.REGEX_WIZARD_MODAL_COMBO_REGEX_TAG, show=True, items=regex_combo_items[selected_base])
+        dpg.set_value(gui_tags.REGEX_WIZARD_MODAL_COMBO_REGEX_TAG, REGEX_COMBO_DEFAULT)
+    
+    def regex_selected() -> None:
+        pass
+
+    with dpg.window(tag=gui_tags.REGEX_WIZARD_MODAL_TAG, width=300, height=450, modal=True, show=False, no_title_bar=True):
+        dpg.add_combo(tag=gui_tags.REGEX_WIZARD_MODAL_COMBO_TYPE_TAG, items=["Armour", "Weapon", "Map"], default_value=ITEM_TYPE_COMBO_DEFAULT, callback=item_type_selected)
+        dpg.add_combo(tag=gui_tags.REGEX_WIZARD_MODAL_COMBO_BASE_TAG, items=[], default_value=ITEM_BASE_COMBO_DEFAULT, show=False, callback=item_base_selected)
+        dpg.add_combo(tag=gui_tags.REGEX_WIZARD_MODAL_COMBO_REGEX_TAG, items=[], default_value=REGEX_COMBO_DEFAULT, show=False, callback=regex_selected)
+
+        with dpg.group(horizontal=True):
+            dpg.add_button(label="OK", callback=lambda: dpg.configure_item(gui_tags.REGEX_WIZARD_MODAL_TAG, show=False))
+            dpg.add_button(label="Cancel", callback=lambda: dpg.configure_item(gui_tags.REGEX_WIZARD_MODAL_TAG, show=False))
+
 def init_gui() -> None:
     dpg.create_context()
     dpg.configure_app(init_file=os.path.join('src', 'gui_layout.ini'), docking=True, docking_space=True)
@@ -79,13 +123,13 @@ def init_gui() -> None:
     except Exception:
         logging.error('Unable to load Bahnschrift font, using default.', exc_info=True)
 
-    dpg.create_viewport(title="Alt-Aug GUI", width=800, height=650)
+    dpg.create_viewport(title="Alt-Aug GUI")
 
     configuration_window = dpg.generate_uuid()
     crafting_window = dpg.generate_uuid()
     script_log = dpg.generate_uuid()
 
-    with dpg.window(tag=configuration_window, label="Configuration"):
+    with dpg.window(tag=configuration_window, label="Configuration", no_close=True):
         dpg.add_text(default_value="Must restart script to apply config changes")
         with dpg.group(horizontal=True):
             dpg.add_button(label="Capture Alteration orb position", callback=capture_alt_position)
@@ -108,16 +152,25 @@ def init_gui() -> None:
                 callback=set_pyautogui_pause,
                 width=100
             )
-            dpg.add_text(default_value="Set PAUSE (Recommended 0.03)")
+            dpg.add_text(default_value="Set PAUSE")
         dpg.add_checkbox(label="Enable performance logging", tag=gui_tags.PERFORMANCE_LOGGING_TAG)
 
-    with dpg.window(tag=crafting_window, label="Crafting goal input"):
-        dpg.add_text("Enter regex for 'Regex' mode (supports poe.re with 'Match an open affix = OFF'):")
-        dpg.add_input_text(tag=gui_tags.REGEX_INPUT_TAG)
-        dpg.add_input_int(tag=gui_tags.MAX_ATTEMPT_INPUT_TAG, label="Max crafting attempts", default_value=10)
-        dpg.add_button(label="Start crafting", callback=crafting_processor.start_crafting)
+    init_regex_wizard_modal()
 
-    with dpg.window(tag=script_log, label="Script log"):
+    with dpg.window(tag=crafting_window, label="Crafting target", no_close=True):
+        with dpg.tab_bar():
+            with dpg.tab(label="Items"):
+                with dpg.group(horizontal=True):
+                    with dpg.group():
+                        dpg.add_text("RegEx input (crafting stops when RegEx matches the item):")
+                        dpg.add_input_text(tag=gui_tags.REGEX_INPUT_TAG)
+                    dpg.add_button(label="Open RegEx\nLibrary", callback=lambda: dpg.configure_item(gui_tags.REGEX_WIZARD_MODAL_TAG, show=True))
+                dpg.add_input_int(tag=gui_tags.MAX_ATTEMPT_INPUT_TAG, label="Max crafting attempts", default_value=10)
+                dpg.add_button(label="Start crafting", callback=crafting_processor.start_crafting)
+            with dpg.tab(label="Maps"):
+                pass
+
+    with dpg.window(tag=script_log, label="Script log", no_close=True):
         with dpg.group(horizontal=True):
             dpg.add_input_text(
                 tag=gui_tags.OUTPUT_LOG_TAG,
